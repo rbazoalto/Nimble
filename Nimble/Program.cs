6d35a079835c7cb60ea18d5a3ac593476182fb4c
Rolando Bazoalto <rbazoalto@engage2excel.com>
2026-01-12 13:48:59 -0400

    2. Allow the application to process entered entries until Ctrl+C is used

--- Nimble/Program.cs
+++ Nimble/Program.cs
@@ -8,11 +8,6 @@ namespace Nimble
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Please add numbers to be added separated by comma or '\\n' char (i.e. 1,2\\n3) Negative numbers are not allowed.");
-            Console.Write("You can also define your own delimiters using the following format (//[##][!!]\\n1##5!!4): ");
-            string input = Console.ReadLine();
-            // We replace the string with the \n char
-            input = input.Replace("\\n", "\n");
             // objects initialization
             InputParser inputParser = new InputParser();
             NumberValidator numberValidator = new NumberValidator();
@@ -20,23 +15,33 @@ namespace Nimble
             int res = 0;
             string message = string.Empty;
 
-            try
+            // We will continue until Ctrl-C is pressed
+            while (true)
             {
-                // First we parse the string to have the list of parameters
-                string[] parsedInput = inputParser.Parse(input);
+                Console.WriteLine("Please add numbers to be added separated by comma or '\\n' char (i.e. 1,2\\n3) Negative numbers are not allowed.");
+                Console.Write("You can also define your own delimiters using the following format (//[##][!!]\\n1##5!!4): ");
+                string input = Console.ReadLine();
+                // We replace the string with the \n char
+                input = input.Replace("\\n", "\n");
 
-                // Then we validate the parameters to get a list of numbers
-                List<int> numbers = numberValidator.Validate(parsedInput);
+                try
+                {
+                    // First we parse the string to have the list of parameters
+                    string[] parsedInput = inputParser.Parse(input);
 
-                // Finally, we calculate the sum of the numbers
-                res = calculator.Sum(numbers, out message);
+                    // Then we validate the parameters to get a list of numbers
+                    List<int> numbers = numberValidator.Validate(parsedInput);
 
-                // Print the result
-                Console.WriteLine(message);
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine("ERROR: {0}.", ex.Message);
+                    // Finally, we calculate the sum of the numbers
+                    res = calculator.Sum(numbers, out message);
+
+                    // Print the result
+                    Console.WriteLine(message);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("ERROR: {0}.", ex.Message);
+                }
             }
         }
     }
